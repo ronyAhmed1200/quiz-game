eef9ab13288e63675f1974e29e4a872e3e8b3cd6
rony <moshiourrahmanrony@gmail.com>
2022-10-29 18:40:49 +0600

    fixed gh-pages deploy issue

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <main>
       <Routes>
-        <Route exact path="/quiz-game" element={<Homepage />} />
+        <Route exact path="/" element={<Homepage />} />
         <Route path="/score" element={<Scorepage />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
